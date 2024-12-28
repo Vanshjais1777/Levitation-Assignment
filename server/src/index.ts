6bf28051ae39d7463da73d219a460b8696f03b17
vansh jaiswal <vanshjais1777@gmail.com>
2024-12-28 22:34:38 +0530

    update CORS configuration to allow specific origin and methods

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,29 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import authRoute from '../src/routes/auth.routes'
-import productsRoute from '../src/routes/product.routes'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoute from "../src/routes/auth.routes";
+import productsRoute from "../src/routes/product.routes";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://levitation-frontend-self.vercel.app",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true,
+  })
+);
 
-app.use(express.json({ limit: "10mb" }))
+app.use(express.json({ limit: "10mb" }));
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
-app.use('/api/auth', authRoute);
-app.use('/api/products', productsRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/products", productsRoute);
 
 app.listen(port, () => {
   console.log(`app is listening on port: http://localhost:${port}`);
-})
+});
