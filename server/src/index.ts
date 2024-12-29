baecdd4433abb3a49451647c917abbf608a7df02
vansh jaiswal <vanshjais1777@gmail.com>
2024-12-29 15:09:35 +0530

    cors solved finally

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const allowedOrigins = [
-  "https://levitation-frontend-self.vercel.app",
+  "https://levitation-assignment-frontend.vercel.app/",
   "http://localhost:5173",
 ];
 
