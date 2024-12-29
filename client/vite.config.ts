e378751c4d10c9a8675c4fc7d6bc0d3e0586d216
vansh jaiswal <vanshjais1777@gmail.com>
2024-12-29 14:29:43 +0530

    feat: add server proxy configuration for API requests in Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure the output directory is 'dist'
+    outDir: "dist", // Ensure the output directory is 'dist'
   },
-})
+  server: {
+    proxy: {
+      "/api": "http://localhost:4000", // Forward API requests to the backend
+    },
+  },
+});
